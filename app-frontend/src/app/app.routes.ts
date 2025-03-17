c82d69c2b292d3c69ff4ea4c6b15555e5af53a92
9601dani <erickmorales201930699@cunoc.edu.gt>
2025-03-16 21:45:45 -0600

    fix: created frontend

--- app-frontend/src/app/app.routes.ts
+++ app-frontend/src/app/app.routes.ts
@@ -1,14 +1,3 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './components/commons/home/home.component';
-import { NotFoundComponent } from './components/commons/not-found/not-found.component';
 
-export const routes: Routes = [
-    {path: '', redirectTo: 'home', pathMatch:'full'},
-    {path: 'home', component:   HomeComponent},
-
-
-    {path: '**', component: NotFoundComponent}
-];
-
-
-export class AppRountingModule {}
+export const routes: Routes = [];
