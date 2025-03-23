f835b246d6e6b90823b59556017d07c7080aec45
9601dani <erickmorales201930699@cunoc.edu.gt>
2025-03-22 21:15:00 -0600

    feat: migrations for db

---
+++ app-backend/ms-suscription/src/main/java/com/codenbugs/ms_suscription/MsSuscriptionApplication.java
@@ -0,0 +1,13 @@
+package com.codenbugs.ms_suscription;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSuscriptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsSuscriptionApplication.class, args);
+	}
+
+}
---
+++ app-backend/ms-suscription/src/test/java/com/codenbugs/ms_suscription/MsSuscriptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codenbugs.ms_suscription;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsSuscriptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
