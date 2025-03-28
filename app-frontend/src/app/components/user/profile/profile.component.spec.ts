814710539f89fe58b77cb122bd72ddb95dc63306
Oscar de León Urizar <oscar99deleon19@gmail.com>
2025-03-28 12:22:35 -0600

    fix test

--- app-frontend/src/app/components/user/profile/profile.component.spec.ts
+++ app-frontend/src/app/components/user/profile/profile.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProfileComponent } from './profile.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
 describe('ProfileComponent', () => {
   let component: ProfileComponent;
@@ -8,9 +12,19 @@ describe('ProfileComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ProfileComponent]
-    })
-    .compileComponents();
+      imports: [ProfileComponent],
+      providers: [provideHttpClient(), provideHttpClientTesting(), {
+        provide: ActivatedRoute,
+        useValue: {
+          params: of({ id: '123' }),
+          snapshot: {
+            paramMap: {
+              get: (key: string) => '123'
+            }
+          }
+        }
+      }],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ProfileComponent);
     component = fixture.componentInstance;
