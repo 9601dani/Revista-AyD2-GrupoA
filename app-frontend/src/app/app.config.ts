17961cfecbe777f20f5cdfec0d6f0772ee00d673
Oscar de León Urizar <oscar99deleon19@gmail.com>
2025-03-30 13:03:14 -0600

    add interceptor

--- app-frontend/src/app/app.config.ts
+++ app-frontend/src/app/app.config.ts
@@ -2,9 +2,19 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { provideHttpClient } from '@angular/common/http';
+import {
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
+import { HTTP_INTERCEPTORS, provideHttpClient, withFetch, withInterceptors, withInterceptorsFromDi } from '@angular/common/http';
+import { Interceptor } from './interceptors/interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()), provideHttpClient()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    provideHttpClient(withFetch(), withInterceptorsFromDi()),
+    {provide: HTTP_INTERCEPTORS, useClass: Interceptor, multi: true}
+  ],
 };
